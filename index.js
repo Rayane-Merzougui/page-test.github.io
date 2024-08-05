03c5a50f6a6f42cad50fd347cd3f30c7fa7969f2
rayane <rayane.merzougui.work@gmail.com>
2024-08-05 12:38:31 +0100

    j'ajoute plusieur page

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+const menuburger = document.getElementById("burger");
+const navLinks = document.querySelector(".nav-link");
+menuburger.addEventListener("click", () =>
+  navLinks.classList.toggle("mobil-menu")
+);
