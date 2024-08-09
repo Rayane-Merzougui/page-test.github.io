9a5ebd7e2164eaddcd97751372eca80ea271cf51
rayane <rayane.merzougui.work@gmail.com>
2024-08-09 15:22:38 +0100

    on ajout la calculatrice est du text

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const navLinks = document.querySelector(".nav-link");
 menuburger.addEventListener("click", () =>
   navLinks.classList.toggle("mobil-menu")
 );
+function insert(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    alert("Expression invalide");
+  }
+}
