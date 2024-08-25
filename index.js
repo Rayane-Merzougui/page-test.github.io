c3961bfffc718f4dd8540bf3ad7c25f50e71cb6b
rayane <rayane.merzougui.work@gmail.com>
2024-08-25 14:40:00 +0100

    form quasi terminer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const menuburger = document.getElementById("burger");
 const navLinks = document.querySelector(".nav-link");
 const boxpop = document.querySelector(".boxpop");
 const exit = document.getElementById("exite");
+const form = document.getElementById("form");
 menuburger.addEventListener("click", () =>
   navLinks.classList.toggle("mobil-menu")
 );
@@ -31,4 +32,7 @@ window.addEventListener("scroll", onScroll);
 exit.addEventListener("click", () => {
   boxpop.style.right = -750 + "px";
   window.removeEventListener("scroll", onScroll);
+});
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 });
