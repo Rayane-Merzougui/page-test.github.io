ea5c08dae2f6530269ab2984f358e85155535559
rayane <rayane.merzougui.work@gmail.com>
2024-10-15 20:51:26 +0100

    j'ai réglé l'erreure

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,10 +1,3 @@
-const { ethers } = require("ethers"); // Assure-toi d'importer ethers
-const { JsonRpcProvider } = ethers; // Créer un fournisseur
-
-const provider = new JsonRpcProvider(
-  "https://mainnet.infura.io/v3/f09260a6b2cf4daab666cf5b454d8cc2"
-);
-
 const menuburger = document.getElementById("burger");
 const navLinks = document.querySelector(".nav-link");
 const boxpop = document.querySelector(".boxpop");
@@ -48,18 +41,4 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 });
 
-const address = "0x74f306D64244319F642BF405054743A5A8400302";
-const main = async () => {
-  try {
-    const balance = await provider.getBalance(address);
-    console.log(
-      `\nETH Balance of ${address} --> ${ethers.utils.formatEther(
-        balance
-      )} ETH\n`
-    );
-  } catch (error) {
-    console.error("Error fetching balance:", error);
-  }
-};
 
-main();
